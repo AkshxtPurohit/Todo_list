80081eda685da6f29cf41a32e569d0cdc34f3b37
AkshxtPurohit <akshtpurohit@gmail.com>
2023-09-14 16:19:01 +0530

    todo

--- app/page.js
+++ app/page.js
@@ -1,95 +1,99 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
+const page = () => {
+  const[tittle,settittle]=useState("");
+  const[dis,setdis]=useState("");
+  const[status,setstatus]=useState("pending");
+  const[task,settask]=useState([]);
+  const[activetask,setactivetask]=useState(null);
+  // setInterval(()=>{
+  //   settime(new Date().toLocaleTimeString());
+  // },1000)
+  const sub=(event)=>{
+    event.preventDefault();
+    // settittle(event.target.tittle.value);
+    // setdis(event.target.dis.value);
+    // setstatus(event.target.status.value);
+    const newTask={
+      Date:new Date().toDateString(),
+    tittle,
+    dis,
+    status
+    }
+    // console.log(tittle,status,dis);
+    // task.push(newTask);
+    settask([...task,newTask]);
+    // console.log(task);
+    settittle("");
+    setdis("");
+    setstatus("pending");
+  };
+ 
+  
+  const DeleteHandler=(index)=>{
+    settask(task.filter((t,i)=> i!==index));
+  }
+  const UpdateHandler=(index)=>{
+      const {tittle,dis,status}=task[index];
+      settittle(tittle);
+      setdis(dis);
+      setstatus(status);
+      setactivetask(index);
+  }
+  const UpdateTask=(e)=>{
+      e.preventDefault();
+      const copytask=[...task];
+      copytask[activetask]={...copytask[activetask],tittle,dis,status};
+      settask(copytask);
+      setactivetask(null);
+      settittle("");
+      setdis("");
+      setstatus("pending");
+  }
+  var tasklist=<h2> Loading...</h2>;
+  if(task.length > 0){
+    tasklist=task.map((task,index)=>(
+      <div key={index}>
+        <br />
+        {task.Date}
+        {task.tittle}
+        {task.dis}
+        {task.status}
+        <button onClick={() => DeleteHandler(index)}>Delete Task</button>
+        <button onClick={() => UpdateHandler(index)}>Update Task</button>
+        <br />
+      </div>
+    ))
+  }
+  // const change=
+  // }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      {/* <h1>{name}</h1> */}
+      <form  >
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <input  onChange={(event)=>{settittle(event.target.value);}} value={tittle} type="text" name="tittle" placeholder='tittle'/>
+        <textarea name="dis" cols="30" rows="10" onChange={(event)=>{setdis(event.target.value);}} value={dis}></textarea>
+        <select name="status" 
+        onChange={(event)=>{setstatus(event.target.value);}} 
+        defaultValue={status}>
+          <option value="due">Due</option>
+          <option value="pending">Pending</option>
+          <option value="completed">Completed</option>
+        </select>
+        {activetask === null ? (<input type="submit" onClick={sub} value="CreateTask" />):(<input type="submit" onClick={UpdateTask} value="UpdateTask" />)}
+      </form>
+      {/* <h2>Tasks</h2>
+      <div id="card">
+        
+      </div> */}
+      {tasklist}
+    </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
+  
 }
+
+
+export default page
